1805f06fbe50055857b9d3787cc6399f5150d522
dds <android_shuai@163.com>
2016-10-10 11:03:24 +0800

    Initial commit

---
+++ app/src/main/java/com/jhs/circulateview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.jhs.circulateview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Kanner kanner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        kanner = (Kanner) findViewById(R.id.kanner);
+        kanner.setImagesUrl(new String[] { "http://img04.muzhiwan.com/2015/06/16/upload_557fd293326f5.jpg",
+                "http://img03.muzhiwan.com/2015/06/05/upload_557165f4850cf.png",
+                "http://img02.muzhiwan.com/2015/06/11/upload_557903dc0f165.jpg",
+                "http://img04.muzhiwan.com/2015/06/05/upload_5571659957d90.png",
+                "http://img03.muzhiwan.com/2015/06/16/upload_557fd2a8da7a3.jpg" });
+    }
+}
